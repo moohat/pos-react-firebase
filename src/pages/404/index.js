213eb755f6d6a95e6e969404b131676764fd276d
taufik m <refizcorp@gmail.com>
2020-04-01 20:11:40 +0700

    create public page

--- src/pages/404/index.js
+++ src/pages/404/index.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from 'react';
 
+//material-ui
+import Container from '@material-ui/core/Container';
+import Paper from '@material-ui/core/Paper';
+import Typography from '@material-ui/core/Typography';
+import {Link} from 'react-router-dom'
+
+// styles
+import useStyles from './styles'
 function NotFound(){
-    return <h1>Halaman NotFound</h1>
+    const classes = useStyles();
+    return (
+        <Container maxWidth="xs" >
+            <Paper className={classes.paper}>
+                <Typography variant="subtitle1">Halaman Tidak ditemukan</Typography>
+                <Typography variant="h3">404</Typography>
+                <Typography component={Link} to="/">Kembali ke Beranda</Typography>
+            </Paper>
+        </Container>
+        )
 }
 
 export default NotFound;
